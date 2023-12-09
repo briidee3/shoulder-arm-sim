65ff5cc900f8b68a4b125183d021815a7e27692a
briidee3 <briidee3@gmail.com>
2023-12-09 18:01:54 -0500

    added notes

--- experimentation/mediapipe/gui.py
+++ experimentation/mediapipe/gui.py
@@ -1,6 +1,9 @@
 # BD 2023
 # Program used as a graphical user interface for use with body force simulations research project
 
+### TODO:
+#   - integrate this thing as a child of the "extrapolation" class
+#       - include callback functions to refer to (and manipulate/work with) the parent (extrapolation) class' data
 
 from tkinter import *
 
@@ -12,4 +15,3 @@ class BodyForcesSimGUI():
 
     # display/show GUI
     def show(self):
-        
