81ee4005d4c936526b850b95dcd506e5560583d7
briidee3 <briidee3@gmail.com>
2023-12-09 17:40:28 -0500

    introduction of GUI class

---
+++ experimentation/mediapipe/gui.py
@@ -0,0 +1,15 @@
+# BD 2023
+# Program used as a graphical user interface for use with body force simulations research project
+
+
+from tkinter import *
+
+
+class BodyForcesSimGUI():
+
+    # initialization
+    def __init__(self) -> None:
+
+    # display/show GUI
+    def show(self):
+        
