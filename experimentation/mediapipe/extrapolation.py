cc481ed1e2ad35772dfca16ea46eef184879a8ca
briidee3 <briidee3@gmail.com>
2024-02-18 14:33:32 -0500

    added comments and error checks throughout

--- experimentation/mediapipe/extrapolation.py
+++ experimentation/mediapipe/extrapolation.py
@@ -442,12 +442,14 @@ class Extrapolate_forces():
 
     # get y axes/depths by order of body parts
     def set_depth(self):
-        for vertices in self.vertex_order:
+        # go thru dictionary of vertex order
+        for vertices in self.vertex_order:  # for each set of vertices denoting body segments
             #print(vertices)
-            for i in enumerate(vertices):
+            for i in enumerate(vertices):   # for each vertex in set of vertices
                 #print(i[1])
-                if i[1] != (vertices[-1]):
+                if i[1] != (vertices[-1]):  # if current vertex isn't the last in the set
                     try:
+                        # calculate depth for vertex pair
                         y_dist_between_vertices = self.get_depth(i[1], i[1] + 1)          # calculate depth
                     except:
                         print("extrapolation.py: Error with get_depth() in set_depth()")
@@ -460,6 +462,7 @@ class Extrapolate_forces():
                         vertex_y = self.mediapipe_data_output[i[1] - 1][1] +  y_dist_between_vertices      # add y depth of anchor (previous node) to current
                     else:
                         vertex_y = y_dist_between_vertices
+                    # set depth in current frame of mediapipe data
                     self.mediapipe_data_output[i[1], 1] = vertex_y
 
 
@@ -468,38 +471,44 @@ class Extrapolate_forces():
 
     # calculate elbow angle
     def calc_elbow_angle(self, right_side = False):
-        # coordinate data for shoulder, elbow, and wrist
-        x = self.mediapipe_data_output[(0 + (int)(right_side)):(5 + (int)(right_side)):2, 0]
-        y = self.mediapipe_data_output[(0 + (int)(right_side)):(5 + (int)(right_side)):2, 1]
-        z = self.mediapipe_data_output[(0 + (int)(right_side)):(5 + (int)(right_side)):2, 2]
+        try:
+            # coordinate data for shoulder, elbow, and wrist
+            x = self.mediapipe_data_output[(0 + (int)(right_side)):(5 + (int)(right_side)):2, 0]
+            y = self.mediapipe_data_output[(0 + (int)(right_side)):(5 + (int)(right_side)):2, 1]
+            z = self.mediapipe_data_output[(0 + (int)(right_side)):(5 + (int)(right_side)):2, 2]
 
-        # calculate vectors for getting angle at elbow
-        vector_a = [(x[0] - x[1]), (y[0] - y[1]), (z[0] - z[1])]
-        vector_b = [(x[2] - x[1]), (y[2] - y[1]), (z[2] - z[1])]
+            # calculate vectors for getting angle at elbow
+            vector_a = [(x[0] - x[1]), (y[0] - y[1]), (z[0] - z[1])]
+            vector_b = [(x[2] - x[1]), (y[2] - y[1]), (z[2] - z[1])]
 
-        # calculate length of arm segments via vector math
-        forearm_length = np.sqrt( (vector_b[0] ** 2) + (vector_b[1] ** 2) + (vector_b[2] ** 2) )
-        upperarm_length = np.sqrt( (vector_a[0] ** 2) + (vector_a[1] ** 2) + (vector_a[2] ** 2) )
+            # calculate length of arm segments via vector math
+            forearm_length = np.sqrt( (vector_b[0] ** 2) + (vector_b[1] ** 2) + (vector_b[2] ** 2) )
+            upperarm_length = np.sqrt( (vector_a[0] ** 2) + (vector_a[1] ** 2) + (vector_a[2] ** 2) )
 
-        # calculate angle at elbow
-        elbow_angle = np.arccos( ( (vector_a[0] * vector_b[0]) + (vector_a[1] * vector_b[1]) + (vector_a[2] * vector_b[2]) ) / (forearm_length * upperarm_length) )
+            # calculate angle at elbow
+            elbow_angle = np.arccos( ( (vector_a[0] * vector_b[0]) + (vector_a[1] * vector_b[1]) + (vector_a[2] * vector_b[2]) ) / (forearm_length * upperarm_length) )
 
-        #print(elbow_angle)
-        return elbow_angle
+            #print(elbow_angle)
+            return elbow_angle
+        except:
+            print("extrapolation.py: ERROR in `calc_elbow_angle()`")
 
     # get spherical coordinates for each of the 3 vertices (bodyparts) of interest
     # vertex_one is the anchor point, and vertex_two is calculated based on its anchor
     def calc_spher_coords(self, vertex_one, vertex_two):    
-        # effectively sets origin to cur_anchor
-        x_diff = self.mediapipe_data_output[vertex_two][0] - self.mediapipe_data_output[vertex_one][0]
-        y_diff = self.mediapipe_data_output[vertex_two][1] - self.mediapipe_data_output[vertex_one][1]
-        z_diff = self.mediapipe_data_output[vertex_two][2] - self.mediapipe_data_output[vertex_one][2]
+        try:
+            # effectively sets origin to cur_anchor
+            x_diff = self.mediapipe_data_output[vertex_two][0] - self.mediapipe_data_output[vertex_one][0]
+            y_diff = self.mediapipe_data_output[vertex_two][1] - self.mediapipe_data_output[vertex_one][1]
+            z_diff = self.mediapipe_data_output[vertex_two][2] - self.mediapipe_data_output[vertex_one][2]
 
-        rho = np.sqrt((x_diff ** 2) + (y_diff ** 2) + (z_diff ** 2))
-        theta = np.arctan(y_diff / x_diff)         # swapped due to equations having different Cartesian coordinate system layout
-        phi = np.arccos(z_diff / rho)
+            rho = np.sqrt((x_diff ** 2) + (y_diff ** 2) + (z_diff ** 2))
+            theta = np.arctan(y_diff / x_diff)         # swapped due to equations having different Cartesian coordinate system layout
+            phi = np.arccos(z_diff / rho)
 
-        return [rho, theta, phi]
+            return [rho, theta, phi]
+        except:
+            print("extrapolation.py: ERROR in `calc_spher_coords(%s, %s)`" % vertex_one, vertex_two)
 
 
 
@@ -507,79 +516,82 @@ class Extrapolate_forces():
 
     # calculate forces of muscle exertions of the arm
     def calc_bicep_force(self):#, is_right = False):   # allow choosing which arm to use
-        # constants, used for organization and readability
-        RHO = 0
-        THETA = 1
-        PHI = 2
-        
-        w_bal = self.ball_mass           # kilograms   # weight of ball
+        try:
+            # constants, used for organization and readability
+            RHO = 0
+            THETA = 1
+            PHI = 2
+            
+            w_bal = self.ball_mass           # kilograms   # weight of ball
 
-        # temp storage for getting both arms in one data frame
-        right_elbow_angle = 0
-        right_bicep_force = 0
-        left_elbow_angle = 0
-        left_bicep_force = 0
+            # temp storage for getting both arms in one data frame
+            right_elbow_angle = 0
+            right_bicep_force = 0
+            left_elbow_angle = 0
+            left_bicep_force = 0
 
-        # run through once for left, once for right
-        for is_right in [0, 1]:
-            # only calculate the following if the elbow angle exists
-            elbow_angle = self.calc_elbow_angle(is_right)
-            if math.isnan(elbow_angle):
-                return math.nan                                         # if elbow_angle == nan, exit function by returning nan
+            # run through once for left, once for right
+            for is_right in [0, 1]:
+                # only calculate the following if the elbow angle exists
+                elbow_angle = self.calc_elbow_angle(is_right)
+                if math.isnan(elbow_angle):
+                    return math.nan                                         # if elbow_angle == nan, exit function by returning nan
 
-            # convert sim units to metric units
-            conv_factor = self.sim_to_real_conversion_factor
+                # convert sim units to metric units
+                conv_factor = self.sim_to_real_conversion_factor
 
-            # get spherical coordinate data for arm segments
-            uarm_spher_coords = self.calc_spher_coords((L_SHOULDER + (int)(is_right)), (L_ELBOW + (int)(is_right)))
-            farm_spher_coords = self.calc_spher_coords((L_ELBOW + (int)(is_right)), (L_WRIST + (int)(is_right)))
+                # get spherical coordinate data for arm segments
+                uarm_spher_coords = self.calc_spher_coords((L_SHOULDER + (int)(is_right)), (L_ELBOW + (int)(is_right)))
+                farm_spher_coords = self.calc_spher_coords((L_ELBOW + (int)(is_right)), (L_WRIST + (int)(is_right)))
 
-            # instead of using averages for segment length, use calculated instead
-            f = farm_spher_coords[RHO] * conv_factor
-            u = uarm_spher_coords[RHO] * conv_factor
-            #print("%0.2f" % f)
-            b = u * 0.11 #0.636                                         # calculated via algebra using pre-existing average proportions data
-            w_fa = self.user_weight * (f * 0.1065)                      # use ratio of f to weight proportion to get weight with calculated f 
-            cgf = 2 * (f ** 2)                                          # calculated via algebra using pre-existing average proportions data
+                # instead of using averages for segment length, use calculated instead
+                f = farm_spher_coords[RHO] * conv_factor
+                u = uarm_spher_coords[RHO] * conv_factor
+                #print("%0.2f" % f)
+                b = u * 0.11 #0.636                                         # calculated via algebra using pre-existing average proportions data
+                w_fa = self.user_weight * (f * 0.1065)                      # use ratio of f to weight proportion to get weight with calculated f 
+                cgf = 2 * (f ** 2)                                          # calculated via algebra using pre-existing average proportions data
 
-            # angles
-            #theta_arm = (np.pi / 2) - farm_spher_coords[THETA]          # angle at shoulder
-            theta_uarm = (np.pi / 2) + uarm_spher_coords[THETA]         # angle of upper arm
-            theta_u = elbow_angle#theta_arm + theta_uarm                # angle at elbow
-            theta_b = np.pi - ( (b - u * np.sin(theta_u)) / np.sqrt( (b ** 2) + (u ** 2) - 2 * b * u * np.sin(theta_u) ) )      # angle at bicep insertion point
-            theta_la = np.cos(theta_uarm) #theta_u - theta_arm - np.pi) #np.sin(theta_uarm)        # angle used for leverage arms fa and bal
+                # angles
+                #theta_arm = (np.pi / 2) - farm_spher_coords[THETA]          # angle at shoulder
+                theta_uarm = (np.pi / 2) + uarm_spher_coords[THETA]         # angle of upper arm
+                theta_u = elbow_angle#theta_arm + theta_uarm                # angle at elbow
+                theta_b = np.pi - ( (b - u * np.sin(theta_u)) / np.sqrt( (b ** 2) + (u ** 2) - 2 * b * u * np.sin(theta_u) ) )      # angle at bicep insertion point
+                theta_la = np.cos(theta_uarm) #theta_u - theta_arm - np.pi) #np.sin(theta_uarm)        # angle used for leverage arms fa and bal
 
-            # lever arms
-            la_fa = cgf * theta_la                                      # forearm lever arm
-            la_bal = f * theta_la                                       # ball lever arm
-            la_bic = b * np.sin(theta_b)                                # bicep lever arm
+                # lever arms
+                la_fa = cgf * theta_la                                      # forearm lever arm
+                la_bal = f * theta_la                                       # ball lever arm
+                la_bic = b * np.sin(theta_b)                                # bicep lever arm
 
-            # forces
-            force_bicep = (w_fa * la_fa + w_bal * la_bal) / la_bic      # force applied by bicep muscle
+                # forces
+                force_bicep = (w_fa * la_fa + w_bal * la_bal) / la_bic      # force applied by bicep muscle
 
-            # handle which arm is currently calculating
-            if is_right:
-                right_elbow_angle = elbow_angle
-                right_bicep_force = force_bicep         # seems to be having issues; may be due to being flipped relative to left, and trig functions don't account for that.
-            else:
-                left_elbow_angle = elbow_angle
-                left_bicep_force = force_bicep
-
-
+                # handle which arm is currently calculating
+                if is_right:
+                    right_elbow_angle = elbow_angle
+                    right_bicep_force = force_bicep         # seems to be having issues; may be due to being flipped relative to left, and trig functions don't account for that.
+                else:
+                    left_elbow_angle = elbow_angle
+                    left_bicep_force = force_bicep
 
 
-        # set/return data in dictionary format
-        calculated_data = {
-            "right_bicep_force": str("%0.2f" % right_bicep_force),
-            "right_elbow_angle": str("%0.2f" % np.rad2deg(right_elbow_angle)),
-            "left_bicep_force": str("%0.2f" % left_bicep_force),
-            "left_elbow_angle": str("%0.2f" % np.rad2deg(left_elbow_angle)),
-            "uarm_spher_coords": str(uarm_spher_coords),
-            "farm_spher_coords": str(farm_spher_coords)
-        }
-        #print("%0.2f" % force_bicep)
 
-        return calculated_data
+
+            # set/return data in dictionary format
+            calculated_data = {
+                "right_bicep_force": str("%0.2f" % right_bicep_force),
+                "right_elbow_angle": str("%0.2f" % np.rad2deg(right_elbow_angle)),
+                "left_bicep_force": str("%0.2f" % left_bicep_force),
+                "left_elbow_angle": str("%0.2f" % np.rad2deg(left_elbow_angle)),
+                "uarm_spher_coords": str(uarm_spher_coords),
+                "farm_spher_coords": str(farm_spher_coords)
+            }
+            #print("%0.2f" % force_bicep)
+
+            return calculated_data
+        except:
+            print("extrapolation.py: ERROR in `calc_bicep_force()`")
 
 
 
@@ -587,16 +599,19 @@ class Extrapolate_forces():
 
     # graph bicep forces
     def plot_bicep_forces(self, bicep_force, elbow_angle):
-        # plot bicep force / theta_u
-        y = np.abs(bicep_force)            # bicep force
-        x = np.rad2deg(elbow_angle)        # angle at elbow
+        try:
+            # plot bicep force / theta_u
+            y = np.abs(bicep_force)            # bicep force
+            x = np.rad2deg(elbow_angle)        # angle at elbow
 
-        # plot
-        plt.scatter(x,y)
-        #plt.scatter(range(0, np.shape(freemocap_3d_body_data[:,0,0])[0] ), y)
-        plt.ylim([0, 1000])     # restrict y axis to between 0 and 1000 (Newtons)
-        plt.xlim([0, 180])      # restrixt x axis to between 0 and 180 (degrees)
+            # plot
+            plt.scatter(x,y)
+            #plt.scatter(range(0, np.shape(freemocap_3d_body_data[:,0,0])[0] ), y)
+            plt.ylim([0, 1000])     # restrict y axis to between 0 and 1000 (Newtons)
+            plt.xlim([0, 180])      # restrixt x axis to between 0 and 180 (degrees)
 
-        # plot bicep forces
-        return plt
+            # plot bicep forces
+            return plt
+        except:
+            print("extrapolation.py: ERROR in plot_bicep_forces()")
 
