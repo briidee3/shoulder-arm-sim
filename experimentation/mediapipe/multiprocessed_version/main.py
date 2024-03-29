dfbe3afb1177f30d78e1e1d8deb3384ce01dc5a8
briidee3 <briidee3@gmail.com>
2024-03-29 16:14:53 -0400

    began setup for managing the processes via main.py

--- experimentation/mediapipe/multiprocessed_version/main.py
+++ experimentation/mediapipe/multiprocessed_version/main.py
@@ -2,7 +2,44 @@
 # This program is intended to serve as the main process for the biomechanics simulation project.abs
 
 
+### TODO:
+#   - make a new class for doing only the physics calculations
+
+
 import multiprocessing
 
-import gui          # custom class, handles gui
-import livestream   # custom class, handles mediapipe live stream
+import gui              # custom class, handles gui
+import livestream       # custom class, handles mediapipe live stream
+import extrapolation    # custom class, handles depth extrapolation (and all other calculations for now)
+
+
+### OPTIONS:
+
+# model to use for mediapipe
+pose_landmarker = '../landmarkers/pose_landmarker_full.task'
+
+# load and prep placeholder image for program initialization
+no_image_path = './no_image.png'            # placeholder image location
+no_image = Image.fromarray(cv2.cvtColor(cv2.imread(no_image_path), cv2.COLOR_BGR2RGB))
+
+
+### ENTRY POINT
+if __name__ == '__main__': 
+    # set multiprocessing start method to 'spawn' to prevent any issues running on windows (where only 'spawn' is available) and linux (where default is 'fork')
+    multiprocessing.set_start_method('spawn')
+
+    # initialize pipes
+    extrap_to_stream_r, extrap_to_stream_w = multiprocessing.Pipe()     # pipe to (live)stream from extrapolation
+    stream_to_extrap_r, stream_to_extrap_w = multiprocessing.Pipe()     # pipe to extrap(olation) from livestream
+    stream_to_gui_r, stream_to_gui_w = multiprocessing.Pipe()           # pipe to gui from (live)stream
+    gui_to_extrap_r, gui_to_extrap_w = multiprocessing.Pipe()           # pipe to extrap(olation) from gui
+
+
+    ### PROCESS INITIALIZATION
+    # make SimGUI object and start it (making this file runnable)
+    gui = SimGUI()
+    gui.start()
+    
+    # intitialize extrapolation process
+    ep = extrapolation.Extrapolate_forces(pipe_to_stream = pipe_to_stream_w, pipe_to_extrap = pipe_to_extrap_r)
+    ep.start()
