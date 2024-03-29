748c84baa7af9c1665bf4df21aaad7dc50302a64
briidee3 <briidee3@gmail.com>
2024-03-29 15:43:35 -0400

    added new main class for acting as the main/parent process for multiprocessing

---
+++ experimentation/mediapipe/multiprocessed_version/main.py
@@ -0,0 +1,8 @@
+# BD 2024
+# This program is intended to serve as the main process for the biomechanics simulation project.abs
+
+
+import multiprocessing
+
+import gui          # custom class, handles gui
+import livestream   # custom class, handles mediapipe live stream
