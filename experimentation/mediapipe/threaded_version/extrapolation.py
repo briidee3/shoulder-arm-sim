3597f456a191539cbe451dd0950a64a7f486c47a
briidee3 <briidee3@gmail.com>
2024-04-20 15:39:14 -0400

    removed unnecessary comment

--- experimentation/mediapipe/threaded_version/extrapolation.py
+++ experimentation/mediapipe/threaded_version/extrapolation.py
@@ -490,10 +490,7 @@ class Extrapolate_forces():
             cur_dist = self.calc_dist_between_vertices(vertex_one, vertex_two)      # current distance between given parts
             
             segment_index = VERTEX_TO_SEGMENT[vertex_one][vertex_two]               # get segment index for getting bodypart length 
-
-            # TODO : put this part into simulation units (currently not)
             max_dist = self.bodypart_lengths[segment_index]                         # set max_dist to true length of given bodypart/segment
-            
             #print("v1 %s, v2 %s, si %s" % (vertex_one, vertex_two, segment_index))  # DEBUG
             angle = self.angle_from_normal((self.sim_to_real_conversion_factor * cur_dist), max_dist)   # calculate difference between max distance and current distance
 
